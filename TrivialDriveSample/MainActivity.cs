15ee1a3ea7e2447265c68ecaae532daf8ec21022
Prashant Cholachagudda <pvc@outlook.com>
2017-01-24 09:49:09 +0530

    Added the mobile center packages

--- TrivialDriveSample/MainActivity.cs
+++ TrivialDriveSample/MainActivity.cs
@@ -1,22 +1,63 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Util;
+using Plugin.InAppBilling;
+
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace TrivialDriveSample
 {
-	[Activity (Label = "Trivial Drive", MainLauncher = true, Icon = "@mipmap/icon")]
+	[Activity(Label = "Trivial Drive", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		
-		protected override void OnCreate (Bundle savedInstanceState)
+
+		protected async override void OnCreate(Bundle savedInstanceState)
 		{
-			base.OnCreate (savedInstanceState);
+			base.OnCreate(savedInstanceState);
+
+			MobileCenter.Start("bb1d7d09-a394-45e0-87ac-57bed83d9904",
+					typeof(Analytics), typeof(Crashes));
 
 			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+			SetContentView(Resource.Layout.Main);
 
+
+
+			LoadData();
+
+			string base64EncodedPublicKey = "CONSTRUCT_YOUR_KEY_AND_PLACE_IT_HERE";
+
+			Log.Debug(TAG, "Creating IAB Helper");
+
+			var connected = await CrossInAppBilling.Current.ConnectAsync();
+
+			System.Console.WriteLine(connected);
 
 		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+		{
+			base.OnActivityResult(requestCode, resultCode, data);
+			InAppBillingImplementation.HandleActivityResult(requestCode, resultCode, data);
+		}
+
+		void LoadData()
+		{
+			var sp = GetPreferences(FileCreationMode.Private);
+			tank = sp.GetInt("tank", 2);
+
+			Log.Debug(TAG, $"Loaded the data: tank = {tank}");
+		}
+
+		int tank; // current amount of gas in the tank, in unit
+
+		const string TAG = "TrivialDrive";
 	}
+
+
 }
 
